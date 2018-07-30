692d12d2130827007e99e097a755a82cf132f781
Agneeth Mazumdar <agneeth.mazumdar@gmail.com>
2018-07-29 22:15:49 -0400

    Create scrapeHN.go

---
+++ scrapeHN.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/PuerkitoBio/goquery"
+)
+
+func scrapeHN() {
+
+	res, err := http.Get("https://news.ycombinator.com/")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != 200 {
+		log.Fatalf("Status code error: %d %s", res.StatusCode, res.Status)
+	}
+
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Find the thread title and urls
+	doc.Find("#hnmain tbody tr .itemlist .athing .title .storylink").Each(func(i int, s *goquery.Selection) {
+		// For each item found, get the title and url
+		title := s.Text()
+		link, _ := s.Attr("href")
+		fmt.Printf("Article %d: %s | %s `\n`", i, title, link)
+	})
+}
+
+func main() {
+	scrapeHN()
+}
